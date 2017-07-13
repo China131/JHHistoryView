44ccfd77cea9cc7eae1f91aece588c82da4f2649
China131 <JHSilkTree@foxmail.com>
2017-07-13 09:25:47 +0800

    fix bugs

--- JHShowHistoryView/JHCustomFlow.m
+++ JHShowHistoryView/JHCustomFlow.m
@@ -41,10 +41,12 @@
         //前一个cell的最右边
         NSInteger origin                                          = CGRectGetMaxX(prevLayoutAttributes.frame);
         //如果当前一个cell的最右边加上我们想要的间距加上当前cell的宽度依然在contentSize中，我们改变当前cell的原点位置
-        //不加这个判断的后果是，UICollectionView只显示一行，原因是下面所有cell的x值都被加到第一行最后一个元素的后面了        
+        //不加这个判断的后果是，UICollectionView只显示一行，原因是下面所有cell的x值都被加到第一行最后一个元素的后面了
+        
         if(origin + maximumSpacing  + currentLayoutAttributes.frame.size.width < self.collectionViewContentSize.width) {
             CGRect frame                  = currentLayoutAttributes.frame;
             frame.origin.x                = origin + maximumSpacing;
+            frame.origin.y = prevLayoutAttributes.frame.origin.y;
             currentLayoutAttributes.frame = frame;
         }else{
             CGRect frame                  = currentLayoutAttributes.frame;
